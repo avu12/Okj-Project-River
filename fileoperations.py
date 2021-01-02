cd3469d87763fa0c48ae27fe70d0f6f77cd813f7
ZsoltBig <zsoltbig1@gmail.com>
2021-01-02 20:04:09 +0100

    Add data to file done

--- fileoperations.py
+++ fileoperations.py
@@ -5,5 +5,16 @@ def create_file():
     f.close()
 
 #TODO
-def add_data_to_file(filename):
-    pass
+def add_data_to_file():
+   filename = input("Give me a filename:")
+   f = open(filename,"a")
+   river_name = input("River name:")
+   river_size = input("River size:")
+   try:
+       river_size = int(river_size)
+   except ValueError as e:
+       print(e)
+       return -1
+
+   f.write(river_name+";"+str(river_size)+"\n")
+   f.close()
