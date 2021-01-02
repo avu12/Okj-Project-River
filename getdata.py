30d050d70e538aa5d2e09eec2b6cbfe994a6a744
doczizoli <zolidoczi@gmail.com>
2021-01-02 19:24:32 +0100

    +  max_river...done

--- getdata.py
+++ getdata.py
@@ -27,11 +27,36 @@ def min_river(filename):
     print("Minimum river name and value:\n" + min_name+ " " + str(min_value) )
 
 
-#TODO
-def max_river(filename):
-    pass
 
-#TODO
+def max_river(filename):
+    folyok = []
+    try:
+        f = open(filename,"r")
+    except OSError as e:
+        print(e)
+        return -1
+   
+    for sor in f:
+        sor = sor[:-1].split(";")
+        sor[1] = int(sor[1])
+        folyok.append(sor)
+    f.close()
+
+    if len(folyok) == 0 :
+        print("Empty file!")
+        return 0
+
+    max_name = folyok[0][0]
+    max_value = folyok[0][1]
+    for f in folyok:
+        if f[1] > max_value:
+            max_value = f[1]
+            max_name  = f[0]
+
+    print("Maximum river name and value:\n" + max_name+ " " + str(max_value) )
+
+
+
 def avg_river(filename):
     pass
 
